588bc4dbed2714d7fe1f9ef18ca382c27b1a4402
Sam Gathercole <sam@nextail.co>
2018-11-22 18:08:33 +0100

    Some log stuff

--- tcr.sh
+++ tcr.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+TEST_OUTPUT=/tmp/tcr_ouput.log
+
 test () {
-  rspec > /tmp/tcr_ouput.log
+  rspec > $TEST_OUTPUT
 }
 
 commit () {
-  cat /tmp/tcr_ouput.log
+  cat $TEST_OUTPUT
   echo "Green!!!"
   git add -A
   git commit -m "$(date +%s)"
