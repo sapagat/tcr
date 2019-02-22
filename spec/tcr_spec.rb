f9f7e06ce7b3f4b66a7a2b01d375c91435cba1af
Sam Gathercole <sam@nextail.co>
2019-02-22 11:30:27 +0100

    1550831427

--- spec/tcr_spec.rb
+++ spec/tcr_spec.rb
@@ -2,7 +2,7 @@ describe 'TCR' do
   it 'works' do
     expect(true).to eq(true)
     expect(false).to eq(false)
-    expect(true).not_to eq(false)
-    expect(false).not_to eq(true)
+    # expect(true).not_to eq(false)
+    # expect(false).not_to eq(true)
   end
 end
